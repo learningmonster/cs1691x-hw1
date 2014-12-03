94f6364b3bb1e05b91c522b601af9d14451663ab
Tayyab Siddiqui <creativemonsterdev@gmail.com>
2014-12-03 08:57:00 -0800

    Part 4 working...

--- ruby-calisthenics/lib/rock_paper_scissors.rb
+++ ruby-calisthenics/lib/rock_paper_scissors.rb
@@ -6,6 +6,11 @@ class RockPaperScissors
   def self.winner(player1, player2)
     # YOUR CODE HERE
   end
+  
+	def test_which_wins?(move1, move2)
+  		#code to determine which move wins
+  		return false
+	end
 
   def self.tournament_winner(tournament)
     # YOUR CODE HERE
